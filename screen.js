1b2b8ce2f2d847484ece7add1e439278bad39cd5
robinromance <robinhipro@gmail.com>
2023-10-20 15:32:48 +0530

    Calculator

---
+++ screen.js
@@ -0,0 +1,26 @@
+let screen = document.getElementById('inputBox')
+    let numbers = document.querySelectorAll('button')
+    let string = '';
+    let arr = Array.from(numbers);
+    arr.forEach(press => {
+        press.addEventListener('click', (e) =>{
+            if(e.target.innerHTML == '='){
+                string = eval(string);
+                screen.value = string
+                console.log(string)
+            }
+            else if(e.target.innerHTML == 'AC'){
+                string = ''
+                screen.value = string
+            }
+            else if(e.target.innerHTML == 'DEL'){
+                string = string.substring(0, string.length-1)
+                screen.value = string
+            }
+            else{
+                string += e.target.innerHTML;
+                screen.value = string;
+            }
+            
+        })
+    })
